bbd661b5822bb8aee18e08093403efd24c5b78af
HTAYHTAYHLAING\HtayHtayHlaing <scm.htayhtayhlaing@gmail.com>
2022-02-15 14:19:04 +0630

    [feature/diamon]Updated some changes

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -10,7 +10,7 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Diamond");
             Console.ReadLine();
         }
     }
